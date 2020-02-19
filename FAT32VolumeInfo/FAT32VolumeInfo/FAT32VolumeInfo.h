9ef008c0dd92a7f28c3c0f47a9558bd01cb3580a
Renat <rinoz1@yandex.ru>
2020-02-19 11:30:38 +0500

    add mbr struct

--- FAT32VolumeInfo/FAT32VolumeInfo/FAT32VolumeInfo.h
+++ FAT32VolumeInfo/FAT32VolumeInfo/FAT32VolumeInfo.h
@@ -6,6 +6,26 @@
 #define FAT32VOLUMEINFO_API __declspec(dllimport)
 #endif
 
+#pragma pack(1)
+typedef struct
+{
+	BYTE emptyBytes[3];
+	BYTE OEM_ASCII[8];
+	WORD bytesPerSector;
+	BYTE clusterFactor;
+	WORD reservedAreaSize;
+	BYTE copyFATCnt;
+	WORD maxCntRecordsFAT1216;
+	WORD sectorsCnt16;
+	BYTE mediaType;
+	WORD sizeFAT1216;
+	WORD sectorsPerTrack;
+	WORD headsCnt;
+	DWORD sectorsBeforeStartSection;
+	DWORD sectorsCnt32;
+} FAT32;
+#pragma pack()
+
 extern "C++" FAT32VOLUMEINFO_API void execute(std::string diskName);
 extern "C++" FAT32VOLUMEINFO_API std::string getVolumeFileSystemName(std::string diskName);
 extern "C++" FAT32VOLUMEINFO_API bool checkVolumeFS(std::string fsName);
